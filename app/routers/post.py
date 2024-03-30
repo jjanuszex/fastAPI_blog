f1bd12899dfa6f9308d198ca1e306395f00e9ae1
Jan Januszex <jjanuszex@gmail.com>
2024-03-30 18:22:50 +0100

    Query Parameters, search something in url etc

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, responses, status, HTTPException, Depends, APIRoute
 from .. import models, schemas, oauth2
 from ..database import get_db
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/posts",
@@ -11,8 +11,10 @@ router = APIRouter(
 
 
 @router.get("/",  response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).all()
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
+              limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    print(limit)
+    post = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return post
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -27,7 +29,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
 
 @router.get("/{id}")
 def get_post(id: int, response: responses.Response, db: Session = Depends(get_db),
-             ucurrent_user: int = Depends(oauth2.get_current_user, )):
+             ucurrent_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
