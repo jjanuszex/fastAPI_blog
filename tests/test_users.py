09165ba323faa1c48efc4024371b9b940a273c9c
Jan Januszex <jjanuszex@gmail.com>
2024-04-11 13:54:29 +0200

    Add pytest fixture and update test cases

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+import pytest
 from app.main import app
 from app import schemas
 from app.config import settings
@@ -14,8 +15,6 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.create_all(bind=engine)
-
 def override_get_db():
     db = TestingSessionLocal()
     try:
@@ -25,27 +24,28 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
-client = TestClient(app)
+@pytest.fixture
+def client():
+    # run our code before we start testing
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield  TestClient(app)
+    # run our code after we finish testing
+    
+    # we can also use alembic instead
+    # from alembic import command
+    # command.upgrade("head")
+    # command.downgrade("base")
 
 
-# Step 1: Set up a local PostgreSQL database for testing.
-# This command creates a new PostgreSQL database named "test_db" on your local machine.
-# You might need to replace "your_username" with your actual PostgreSQL username.
-
-
-# Step 2: Update your test configuration to use the URL of your local PostgreSQL database.
-# Replace "your_username" and "your_password" with your actual PostgreSQL username and password.
-SQLALCHEMY_DATABASE_URL = "postgresql://your_username:your_password@localhost/test_db"
-
-
-def test_root():
+def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "ser"}
 
-def test_create_users():
+def test_create_users(client):
     res = client.post(
-        "/users/", json={"email": "test2@dupa.com", "password": "password123"})
+        "/users/", json={"email": "test33@dupa.com", "password": "password123"})
     new_user = schemas.UserResponse(**res.json())
     assert res.status_code == 201
-    assert new_user.email == "test2@dupa.com"
+    assert new_user.email == "test33@dupa.com"
